1809e92beef1521f1134e0518f8acb23eb2b0fdb
TigranBalayan10 <tigranbalayan10@gmail.com>
2024-07-30 00:05:46 -0700

    added clerk implemantation with default components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl font-bold">Hello World!</h1>
-      <Button>Click me</Button>
+      <h1>Home</h1>
     </main>
   );
 }
